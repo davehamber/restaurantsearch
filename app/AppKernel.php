2a39547f1f00e78933cfa88703d99edf2dfdd3d3
davehamber <dave.hamber@gmail.com>
2016-01-26 16:15:40 +0100

    Added hwi oauth and api logger bundles to AppKernel.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new DaveHamber\RestaurantSearchBundle\RestaurantSearchBundle(),
-            new FOS\UserBundle\FOSUserBundle()
+            new FOS\UserBundle\FOSUserBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -25,6 +26,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Nixilla\Api\LoggerBundle\NixillaApiLoggerBundle();
         }
 
         return $bundles;
