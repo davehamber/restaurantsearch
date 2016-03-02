c4c5fd437bc0634b2bcb9f9a3779b43ebeabfab2
davehamber <dave.hamber@gmail.com>
2016-03-02 15:11:44 +0100

    Tided up class, moving output from model to view.

--- src/DaveHamber/RestaurantSearchBundle/Model/GooglePlacesViaAddress.php
+++ src/DaveHamber/RestaurantSearchBundle/Model/GooglePlacesViaAddress.php
@@ -4,59 +4,109 @@ namespace DaveHamber\RestaurantSearchBundle\Model;
 
 use joshtronic\GooglePlaces;
 use \GoogleGeocode;
+use Psr\Log\LoggerInterface;
 
-class GooglePlacesViaAddress {
-    
+class GooglePlacesViaAddress
+{
+    /**
+     * @var string
+     */
     private $googleAPIKey;
 
-    private $logger;
-    
     /**
-     * Constructor.
-     *
+     * @var LoggerInterface
      */
-    public function __construct($googleAPIKey, $logger)
+    private $logger;
+
+    /**
+     * @var string
+     */
+    private $rootDir;
+
+    /**
+     * @var string
+     */
+    private $streetViewPath;
+
+    /**
+     * @var GooglePlaces
+     */
+    private $googlePlaces;
+
+    /**
+     * GooglePlacesViaAddress constructor.
+     * @param $googleAPIKey
+     * @param $rootDir
+     * @param $logger
+     */
+    public function __construct($googleAPIKey, $rootDir, $streetViewPath, $logger)
     {
         $this->googleAPIKey = $googleAPIKey;
+        $this->rootDir = $rootDir;
+        $this->streetViewPath = $streetViewPath;
         $this->logger = $logger;
     }
-    
-    public function givePlacesData($searchAddress)
+
+    /**
+     * @param $searchAddress
+     * @return NearBySearchResults
+     */
+    public function getPlacesData($searchAddress)
     {
-        $searchData = null;
-        
+        // converts an address string inputted by the user into a geographical longitude and latitude
         $geo = new GoogleGeocode();
         $geometryData = $geo->geocode($searchAddress);
-        
-        if ($geometryData['Response']['Status'] == 'OK')
-        {
-        
-            $latitude = $geometryData['Geometry']['Latitude'];
-            $longitude = $geometryData['Geometry']['Longitude'];
-        
-            $googlePlaces = new GooglePlaces($this->googleAPIKey);
-        
-            $googlePlaces->query = "restaurant";
-            $googlePlaces->location = array($latitude, $longitude);
-        
-            //$googlePlaces->keyword = "Some address";
-            $googlePlaces->types = "restaurant";
-            $googlePlaces->radius = 500;
-        
-            $searchData = $googlePlaces->textSearch();
-            //searchData("html_attributions", "next_page_token", "results", "status");
-            //searchData["results"](0 - 19)
-            // The id and reference fields are deprecated as of June 24, 2014. They are replaced by the new place ID
-            // Place Search request: 
-            //searchData["results"][n]("formatted_address", "geometry", "icon", "id", "name", "opening_hours", "place_id", "price_level", "reference", "types" )
-            // can perform a Place Details request
-            
-            //$this->logger->info("PLACES DATA: " . implode(", ", array_keys($searchData["results"][0])));
-            $searchData = nl2br(var_export($searchData, true));
-            $searchData = str_replace(' ','&nbsp;', $searchData);
 
+        if ($geometryData['Response']['Status'] != 'OK') {
+            return new NearBySearchResults($this->googleAPIKey, $this);
         }
 
-        return $searchData;
+        $latitude = $geometryData['Geometry']['Latitude'];
+        $longitude = $geometryData['Geometry']['Longitude'];
+
+        // create and prepare the parameters of a GooglePlaces object
+        $this->googlePlaces = new GooglePlaces($this->googleAPIKey);
+
+        $this->googlePlaces->location = array($latitude, $longitude);
+        $this->googlePlaces->types = "restaurant";
+        $this->googlePlaces->rankby = 'distance';
+
+        // create the search result object and then execute search to populate it
+        $searchResults = new NearBySearchResults($this->googleAPIKey, $this);
+        $searchResults->performNearBySearch();
+
+        return $searchResults;
+    }
+
+    /**
+     * @return GooglePlaces
+     */
+    public function getGooglePlaces()
+    {
+        return $this->googlePlaces;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleAPIKey()
+    {
+        return $this->googleAPIKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return $this->rootDir;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetViewPath()
+    {
+        return $this->streetViewPath;
     }
 }
