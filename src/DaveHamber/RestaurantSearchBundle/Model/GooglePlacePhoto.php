a36c965f071965673bbdb817ca0578b741c9ba15
davehamber <dave.hamber@gmail.com>
2016-04-12 17:00:13 +0200

    Modified class to be concerned with only storing the photo reference id.

--- src/DaveHamber/RestaurantSearchBundle/Model/GooglePlacePhoto.php
+++ src/DaveHamber/RestaurantSearchBundle/Model/GooglePlacePhoto.php
@@ -29,77 +29,21 @@ class GooglePlacePhoto
     protected $key;
 
     /**
-     * @var int
-     *
-     * The height of the image available
-     */
-    protected $height = 0;
-
-    /**
-     * @var array
-     * array of strings
-     *
-     * Contains any required attributions. This field will always be present, but may be empty.
-     */
-    protected $htmlAttributions = array();
-
-    /**
      * @var string
      *
      * A string used to identify the photo when you perform a Photo request.
      */
     protected $photoReference = "";
 
-    /**
-     * @var int
-     *
-     * The width of the image available
-     */
-    protected $width = 0;
 
-    /**
-     * GooglePlacePhoto constructor.
-     * @param $key
-     * @param $pathAndFileName
-     * @param array $photoData
-     *
-     * Data is populated from result data from google place search
-     */
-    public function __construct($key, $pathAndFileName, array $photoData)
+    public function __construct($key, $pathAndFileName, $photoReference)
     {
 
         $this->key = $key;
 
         $this->pathAndFileName = $pathAndFileName;
 
-        if (isset($photoData['height'])) {
-            $this->height = (int)$photoData['height'];
-        }
-
-        if (isset($photoData['html_attributions'])) {
-            $this->htmlAttributions = (array)$photoData['html_attributions'];
-        }
-
-        if (isset($photoData['photo_reference'])) {
-            $this->photoReference = (string)$photoData['photo_reference'];
-        }
-
-        if (isset($photoData['width'])) {
-            $this->width = (int)$photoData['width'];
-        }
-    }
-
-    /**
-     * @param int $height
-     * @param int $width
-     *
-     * When the photo object is made by the data returned from the google place search, the photo's dimensions are
-     * given. When we want to fetch the photo
-     */
-    public function setHeightAndWidth($height = 200, $width = 250)
-    {
-        $this->height = $height;
-        $this->width = $width;
+        $this->photoReference = $photoReference;
     }
 
     /**
@@ -112,11 +56,6 @@ class GooglePlacePhoto
      */
     public function getPlacePhoto($height = 0, $width = 0)
     {
-        if (0 == $height || 0 == $width) {
-            $height = $this->height;
-            $width = $this->width;
-        }
-
         $queryString = "?maxwidth=".$width."&maxheight=".$height;
         $queryString .= "&photoreference=".$this->photoReference;
         $queryString .= "&key=".$this->key;
